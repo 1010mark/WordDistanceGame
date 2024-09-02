542d7c2b75638ea155d2756462e1f16fd48383e6
hottaakifumi <hotta.akifumi20050811@keio.jp>
2024-09-02 16:53:18 +0900

    htmlファイル表示

--- app/app.py
+++ app/app.py
@@ -3,9 +3,13 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
 
 
 if __name__ == "__main__":
