338bee9a0e3af4754cd49d010ffd4758f476d856
1010mark <markmentaro@yahoo.co.jp>
2024-09-03 10:41:45 +0900

    init page and add ajax

---
+++ app/static/js/play.js
@@ -0,0 +1,18 @@
+document.getElementById('response-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let answerText = document.getElementById('answer').value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/score?answer=' + encodeURIComponent(answerText), true);
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            var score = xhr.responseText.trim();
+            document.getElementById('last-response').textContent = answerText;
+            document.getElementById('score').textContent = score;
+            document.getElementById('response').value = '';
+        } else {
+            alert('通信に失敗しました。');
+        }
+    };
+    xhr.onerror = () => alert('通信に失敗しました。');
+    xhr.send();
+});
